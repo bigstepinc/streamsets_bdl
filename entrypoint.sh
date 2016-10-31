e8b60693f415da0ec291bb8f148226df95ebda3b
cristina-grosu <cristina.grosu@bigstep.com>
2016-10-31 19:01:25 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ while [ $no_instances -lt $NO ] ; do
 	nslookup $HOSTNAME > $KAFKA_HOME/config/kafka.cluster
 	no_instances=$(($(wc -l < kafka.cluster) - 2))
 done
-nslookup $HOSTNAME > $KAFKA_HOME/config/kafka.cluster
+#nslookup $HOSTNAME > $KAFKA_HOME/config/kafka.cluster
 # Determine the local ip
 ifconfig | grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" >> output
 local_ip=$(head -n1 output) && \
