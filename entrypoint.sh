b353e2d26fd2f2deb6b9e8c4f2717e506171197d
cristina-grosu <cristina.grosu@bigstep.com>
2016-11-17 13:09:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-#Setup environment
+# Setup environment
+# Determine the private ip of the container
 ifconfig | grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b" >> output
 local_ip=$(head -n1 output)
 rm output
-
-
-echo "my local ip is $local_ip"
 
 # Retrieve the instances in the Kafka cluster
 mkdir /tmp/zookeeper && mkdir /tmp/kafka-logs
 
 cd $KAFKA_HOME && \
 cd ./config 
-touch hosts
 
+# Retrive Zookeeper cluster configuration
+touch hosts
 sleep 5
 nslookup $HOSTNAME_ZOOKEEPER >> zk.cluster
 
-echo "the zookeeper cluster is the following one"
-cat zk.cluster
-
-# Configure Zookeeper
 NO=$(($(wc -l < zk.cluster) - 2))
 
 while read line; do
@@ -33,29 +28,23 @@ rm zk.cluster
 sort -n zk.cluster.tmp > zk.cluster.tmp.sort
 mv zk.cluster.tmp.sort zk.cluster.tmp
 
+# Configure zookeeper fields from the Kafka cluster
 no_instances=1
 while read line; do
         if [ "$line" != "" ]; then
-		#eval var=\$"HOST"$no_instances
 		myindex=$(echo $line | sed -e 's/\.//g')
 		echo "server.$myindex=$line:2888:3888" >> $KAFKA_HOME/config/zookeeper.properties
 		echo "$(cat hosts) $line:2181" >  hosts
 		no_instances=$(($no_instances + 1))
 	fi
 done < 'zk.cluster.tmp'
+rm zk.cluster.tmp
 
+# Retrive the components of the Kafka Cluster
 index=0
-
 nslookup $HOSTNAME_KAFKA >> kafka.cluster
 
 NOK=$(($(wc -l < kafka.cluster) - 2))
-
-echo "NOK is $NOK"
-cat kafka.cluster
-
-
-# Configure 
-#NO=$(($(wc -l < kafka.cluster) - 2))
 
 while read line; do
 	ip=$(echo $line | grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b")
@@ -71,14 +60,12 @@ index=1
 while read line; do
 	if [ "$line" != "" ]; then
 		if [ "$line" == "$local_ip" ]; then
-			#echo "$index" >> /tmp/zookeeper/myid
-			current_index=$index
-			echo "my current index is $current_index"
-			cp $KAFKA_HOME/config/server.properties $KAFKA_HOME/config/server-$index.properties
-			sed "s/broker.id=0/broker.id=$index/" $KAFKA_HOME/config/server-$index.properties >> $KAFKA_HOME/config/server-$index.properties.tmp
-			mv $KAFKA_HOME/config/server-$index.properties.tmp $KAFKA_HOME/config/server-$index.properties
-		else
-			index=$(($index + 1))
+			current_index=$(echo $local_ip | sed -e 's/\.//g')
+			cp $KAFKA_HOME/config/server.properties $KAFKA_HOME/config/server-$current_index.properties
+			sed "s/broker.id=0/broker.id=$index/" $KAFKA_HOME/config/server-$current_index.properties >> $KAFKA_HOME/config/server-$current_index.properties.tmp
+			mv $KAFKA_HOME/config/server-$current_index.properties.tmp $KAFKA_HOME/config/server-$current_index.properties
+		#else
+		#	index=$(($index + 1))
 		fi
 	fi
 done < 'kafka.cluster.tmp'
@@ -91,63 +78,60 @@ sed -e 's/\s/,/g' hosts > hosts.txt
 
 content=$(cat $KAFKA_HOME/config/hosts.txt)
 
-index=1
+#index=1
 
-while [ $index -le $NOK ]; do
-	echo "index is $index and current index is $current_index"
-	if [ $index == $current_index ] ; then
-		echo "modific acum zookeeper connect"
-		sed "s/zookeeper.connect=localhost:2181/zookeeper.connect=$content/" $KAFKA_HOME/config/server-$index.properties >> $KAFKA_HOME/config/server-$index.properties.tmp && \
-		mv  $KAFKA_HOME/config/server-$index.properties.tmp  $KAFKA_HOME/config/server-$index.properties
+#while [ $index -le $NOK ]; do
+#	echo "index is $index and current index is $current_index"
+#	if [ $index == $current_index ] ; then
+#		echo "modific acum zookeeper connect"
+		sed "s/zookeeper.connect=localhost:2181/zookeeper.connect=$content/" $KAFKA_HOME/config/server-$current_index.properties >> $KAFKA_HOME/config/server-$current_index.properties.tmp && \
+		mv  $KAFKA_HOME/config/server-$current_index.properties.tmp  $KAFKA_HOME/config/server-$current_index.properties
+		
+		# Start Kafka service
+		$KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server-${current_index}.properties
+	#fi
+	#index=$(($index + 1))
+#done
+rm hosts
+#touch hosts 
 
-# Start Zookeeper service
-#nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
+#if [ "$HOSTNAME_ZOOKEEPER" != "" ]; then
+#	sleep 5
+#	nslookup $HOSTNAME_ZOOKEEPER >> zk.cluster
 
-# Start Kafka service
-		$KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server-${index}.properties
-	fi
-	index=$(($index + 1))
-done
-
-touch hosts 
-
-if [ "$HOSTNAME_ZOOKEEPER" != "" ]; then
-	sleep 5
-	nslookup $HOSTNAME_ZOOKEEPER >> zk.cluster
-
-	echo "the zookeeper cluster is the following one"
-	cat zk.cluster
+#	echo "the zookeeper cluster is the following one"
+#	cat zk.cluster
 
 	# Configure Zookeeper
-	NO=$(($(wc -l < zk.cluster) - 2))
+#	NO=$(($(wc -l < zk.cluster) - 2))
 
-	while read line; do
-		ip=$(echo $line | grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b")
-		echo "$ip" >> zk.cluster.tmp
-	done < 'zk.cluster'
-	rm zk.cluster
+#	while read line; do
+#		ip=$(echo $line | grep -oE "\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b")
+#		echo "$ip" >> zk.cluster.tmp
+#	done < 'zk.cluster'
+#	rm zk.cluster
 
-	sort -n zk.cluster.tmp > zk.cluster.tmp.sort
-	mv zk.cluster.tmp.sort zk.cluster.tmp
+#	sort -n zk.cluster.tmp > zk.cluster.tmp.sort
+#	mv zk.cluster.tmp.sort zk.cluster.tmp
 
-	no_instances=1
-	while read line; do
-        	if [ "$line" != "" ]; then
-			echo "$(cat hosts) $line:2181" >  hosts
-			no_instances=$(($no_instances + 1))
-		fi
-	done < 'zk.cluster.tmp'
+#	no_instances=1
+#	while read line; do
+#        	if [ "$line" != "" ]; then
+#			echo "$(cat hosts) $line:2181" >  hosts
+#			no_instances=$(($no_instances + 1))
+#		fi
+#	done < 'zk.cluster.tmp'
 
-fi
+#fi
 
-sed -i 's/^ *//' hosts 
-sed -e 's/\s/,/g' hosts > hosts.txt
+#sed -i 's/^ *//' hosts 
+#sed -e 's/\s/,/g' hosts > hosts.txt
 
-content=$(cat hosts.txt)
+#content=$(cat hosts.txt)
 ZKHOSTS=$content
 
 rm hosts
-rm hosts.txt
+#rm hosts.txt
 
-echo $ZKHOSTS
+#echo $ZKHOSTS
 $KAFKA_MANAGER_HOME/bin/kafka-manager -Dkafka-manager.zkhosts=$ZKHOSTS
