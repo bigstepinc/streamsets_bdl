00cb4f51a52928e330faa355802f8d8c2b9c6933
cristina-grosu <cristina.grosu@bigstep.com>
2016-12-08 21:14:55 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -159,16 +159,19 @@ while read line; do
 		mv  $KAFKA_HOME/config/server-$index.properties.tmp  $KAFKA_HOME/config/server-$index.properties
 		
 		if [ "$KAFKA_PATH" != "" ]; then
-			path1=$(echo $KAFKA_PATH | tr "\\" " " | awk '{ print $1 }')
-			path2=$(echo $KAFKA_PATH | tr "\\" " " | awk '{ print $2 }')
-			path3=$(echo $KAFKA_PATH | tr "\\" " " | awk '{ print $3 }')
-			path=$path1$path2$path3
-			cd $path && mkdir kafka-logs-$HOSTNAME_KAFKA
-			sed "s/log.dirs.*/log.dirs=$KAFKA_PATH\/kafka-logs-$HOSTNAME_KAFKA/"  $KAFKA_HOME/config/server-$index.properties >>  $KAFKA_HOME/config/server-$index.properties.tmp &&
-        		mv  $KAFKA_HOME/config/server-$index.properties.tmp  $KAFKA_HOME/config/server-$index.properties
+			#path1=$(echo $KAFKA_PATH | tr "\\" " " | awk '{ print $1 }')
+			#path2=$(echo $KAFKA_PATH | tr "\\" " " | awk '{ print $2 }')
+			#path3=$(echo $KAFKA_PATH | tr "\\" " " | awk '{ print $3 }')
+			#path=$path1$path2$path3
+			#cd $path && mkdir kafka-logs-$HOSTNAME_KAFKA
+			cd $path && mkdir kafka-logs-$index
+			#sed "s/log.dirs.*/log.dirs=$KAFKA_PATH\/kafka-logs-$HOSTNAME_KAFKA/"  $KAFKA_HOME/config/server-$index.properties >>  $KAFKA_HOME/config/server-$index.properties.tmp &&
+        		sed "s/log.dirs.*/log.dirs=$KAFKA_PATH\/kafka-logs-$index/"  $KAFKA_HOME/config/server-$index.properties >>  $KAFKA_HOME/config/server-$index.properties.tmp &&
+			mv  $KAFKA_HOME/config/server-$index.properties.tmp  $KAFKA_HOME/config/server-$index.properties
 		fi
 		
-		path=$path"/kakfa-logs-$HOSTNAME_KAFKA/.lock"
+		#path=$path"/kakfa-logs-$HOSTNAME_KAFKA/.lock"
+		path=$path"/kakfa-logs-$index/.lock"
 		rm $path
 
 		# Start Kafka Manager Service
